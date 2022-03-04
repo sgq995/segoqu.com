6618be6db5f74051ebe6267b29e88721c9fad308
Sebastian G. Q <sgq995@gmail.com>
2022-03-03 19:26:43 -0500

    [FEAT] Certbot added to nginx

---
+++ nginx/certbot-init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+domains() {
+  domain_list=$(
+    ls /etc/nginx/conf.d | sort -n | sed 's/.*/\/etc\/nginx\/conf.d\/&/' \
+    | xargs grep -RiIh server_name \
+    | sed -e ':a;N;$!ba;s/\([^;\{\}]\)\n/\1 /g' | grep -E 'server_name[ \s\t]' \
+    | grep -v '\$' | grep '\.' \
+    | sed -r -e 's/(\S)[ \t]+(\S)/\1\n\2/g' -e 's/[\t ]//g' -e 's/;//' -e 's/server_name//' \
+    | sed '/^[[:space:]]*$/d' | uniq | sed 's/.*/-d &/' | tr '\n' ' '
+  )
+}
+
+main() {
+  domains
+
+  echo $domain_list \
+    | xargs certbot --nginx \
+      -m "sgq995@gmail.com" \
+      --agree-tos \
+      --eff-email 
+      # --hsts \
+      # --uir
+}
+
+until [ -f /var/run/nginx.pid ]
+do
+  sleep 5
+done
+
+main
