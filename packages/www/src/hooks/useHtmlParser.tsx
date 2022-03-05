d16803af6c651200495aca695015ec1ba0182453
Sebastian G. Q <sgq995@gmail.com>
2022-03-05 18:11:50 -0500

    [FEAT] Added headings

--- packages/www/src/hooks/useHtmlParser.tsx
+++ packages/www/src/hooks/useHtmlParser.tsx
@@ -1,14 +1,27 @@
 import { ReactNode, useEffect, useRef, useState } from "react";
 
 import Link from "@mui/material/Link";
+import Typography, { TypographyProps } from "@mui/material/Typography";
 
 import Paragraph from "../components/Paragraph";
 
 import { HTMLParser, HTMLParserFactory } from "../utils/react-html";
 
+function HeadingFactory(variant: TypographyProps["variant"]) {
+  return (props: TypographyProps) => {
+    return <Typography variant={variant} {...props} />;
+  };
+}
+
 const mapHTMLToReact = {
   p: Paragraph,
   a: Link,
+  h1: HeadingFactory("h1"),
+  h2: HeadingFactory("h2"),
+  h3: HeadingFactory("h3"),
+  h4: HeadingFactory("h4"),
+  h5: HeadingFactory("h5"),
+  h6: HeadingFactory("h6"),
 };
 
 const useHtmlParser = (content: string) => {
