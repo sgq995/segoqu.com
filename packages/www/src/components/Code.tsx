8457332f525cd6345f71b60785b3e2cadea0a5a7
Sebastian G. Q <sgq995@gmail.com>
2022-03-11 19:14:06 -0500

    [CHORE] Removed unused code

--- packages/www/src/components/Code.tsx
+++ packages/www/src/components/Code.tsx
@@ -10,62 +10,22 @@ function instanceofReactElement(object: any): object is ReactElement {
 }
 
 const Code: NextComponentType = ({ children }: React.PropsWithChildren<{}>) => {
-  try {
-    const code = React.Children.toArray(children)[0];
+  const code = React.Children.toArray(children)[0];
 
-    if (instanceofReactElement(code)) {
-      const { children: codeChildren, ...props } = code.props;
+  if (instanceofReactElement(code)) {
+    const { children: codeChildren, ...props } = code.props;
 
-      const content = React.Children.toArray(codeChildren)
-        .flatMap((child) => {
-          console.group("map");
-          console.log({ child });
-          console.groupEnd();
-          if (typeof child === "string") {
-            return child.split("\n");
-          } else {
-            return null;
-          }
-        })
-        .filter((child) => {
-          console.group("filter");
-          console.log({ child });
-          console.groupEnd();
-          return typeof child === "string";
-        })
-        .reduce<React.ReactNode[]>((contentChildren, child, index, array) => {
-          console.group("reduce");
-          console.log({ child });
-          console.groupEnd();
-          
-          if (index > 0 && index < array.length) {
-            return contentChildren.concat(
-              React.createElement("br", { "data-rich-text-line-break": true }),
-              React.createElement("span", {}, child as string)
-            );
-          } else {
-            return contentChildren.concat(
-              React.createElement("span", {}, child as string)
-            );
-          }
-        }, []);
-
-      console.log({ codeChildren, content });
-
-      return (
-        <Paper component="pre" variant="outlined">
-          <Box
-            sx={{ p: 2, display: "inline-block" }}
-            component={code.type}
-            {...props}
-          >
-            {codeChildren}
-          </Box>
-        </Paper>
-      );
-    }
-  } catch (err) {
-    console.error(err);
+    return (
+      <Paper component="pre" variant="outlined">
+        <Box
+          sx={{ p: 2, display: "inline-block" }}
+          component={code.type}
+          {...props}
+        >
+          {codeChildren}
+        </Box>
+      </Paper>
+    );
   }
 
   return <></>;
