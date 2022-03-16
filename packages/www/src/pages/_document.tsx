c015f2ad8f41b30a91f7d96c5215cee8255e2738
Sebastian G. Q <sgq995@gmail.com>
2022-03-16 18:15:03 -0500

    [FEAT] Added font style

--- packages/www/src/pages/_document.tsx
+++ packages/www/src/pages/_document.tsx
@@ -70,6 +70,17 @@ export default class MyDocument extends Document {
             sizes="32x32"
           />
 
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Code+Pro:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            rel="stylesheet"
+          />
+
           {(this.props as any).emotionStyleTags}
         </Head>
         <body>
--- packages/www/src/theme.ts
+++ packages/www/src/theme.ts
@@ -1,5 +1,47 @@
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontFamily: "'Merriweather', serif",
+    },
+    h2: {
+      fontFamily: "'Merriweather', serif;",
+    },
+    h3: {
+      fontFamily: "'Merriweather', serif;",
+    },
+    h4: {
+      fontFamily: "'Merriweather', serif;",
+    },
+    h5: {
+      fontFamily: "'Merriweather', serif;",
+    },
+    h6: {
+      fontFamily: "'Merriweather', serif;",
+    },
+    subtitle1: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    subtitle2: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    body1: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    body2: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    button: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    caption: {
+      fontFamily: "'Lato', sans-serif",
+    },
+    overline: {
+      fontFamily: "'Lato', sans-serif",
+    },
+  },
+});
 
 export default theme;
